5329de802ff20b62faaee6166ca4ee06d41edd43
SniffingTiger <SniffingTiger@users.noreply.github.com>
2018-09-25 20:46:24 -0400

    Added a local project DB called 'RusticsDB 'to work with.

---
+++ RusticsDB/dbo/Tables/Materials.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Materials] (
+    [MatID]           INT        NOT NULL,
+    [Description]     NCHAR (10) NOT NULL,
+    [Color]           NCHAR (10) NULL,
+    [Price]           MONEY      NULL,
+    [SourceCompID]    INT        NULL,
+    [AmountAvailable] NCHAR (10) NULL,
+    CONSTRAINT [PK_Materials] PRIMARY KEY CLUSTERED ([MatID] ASC),
+    CONSTRAINT [FK_Materials_SourceCompany] FOREIGN KEY ([SourceCompID]) REFERENCES [dbo].[Supplier] ([SourceCompID])
+);
+
---
+++ RusticsDB/dbo/Tables/ProductMaterial.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[ProductMaterial] (
+    [ProductID]    INT NOT NULL,
+    [MatID]        INT NOT NULL,
+    [ProductMatID] INT IDENTITY (1, 1) NOT NULL,
+    CONSTRAINT [PK_ProductMaterials_1] PRIMARY KEY CLUSTERED ([ProductMatID] ASC),
+    CONSTRAINT [FK_ProductMaterials_Products] FOREIGN KEY ([ProductID]) REFERENCES [dbo].[Product] ([ProductID]),
+    CONSTRAINT [FK_ProductMaterials_to_Materials] FOREIGN KEY ([MatID]) REFERENCES [dbo].[Materials] ([MatID])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [NonClusteredIndex-ByProductID]
+    ON [dbo].[ProductMaterial]([ProductID] ASC, [ProductMatID] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [NonClusteredIndex-ByMatID]
+    ON [dbo].[ProductMaterial]([MatID] ASC, [ProductMatID] ASC);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'FK ''MatID'' in dbo.ProductMaterials links to the PK ''MatID'' in dbo.Materials', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ProductMaterial', @level2type = N'CONSTRAINT', @level2name = N'FK_ProductMaterials_to_Materials';
+
