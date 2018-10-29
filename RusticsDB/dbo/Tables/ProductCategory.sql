951f6739cb1a81b45a5d464bbb4a7ff3ca9bc589
SniffingTiger <SniffingTiger@users.noreply.github.com>
2018-10-29 19:56:20 -0400

    Delete Windows Forms and DB projects from solution

--- RusticsDB/dbo/Tables/ProductCategory.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[ProductCategory] (
-    [CategoryID] INT           IDENTITY (1, 1) NOT NULL,
-    [Category]   NVARCHAR (30) NOT NULL,
-    CONSTRAINT [PK_ProductCategories] PRIMARY KEY CLUSTERED ([CategoryID] ASC)
-);
-
