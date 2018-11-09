39e0b9f92754f631657befa8ac1641e0a3975765
SniffingTiger <SniffingTiger@users.noreply.github.com>
2018-11-09 09:19:06 -0500

    Add Test Method for creating a product This test method tests whether the Create() method in ProductsController returns a valid ActionResult

---
+++ Rustics_Unit_Tests/ProductsTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RusticsInventoryMVC.Controllers;
+using System.Web.Mvc;
+using RusticsInventoryMVC.Models;
+using Moq;
+
+namespace Rustics_Unit_Tests
+{
+    [TestClass]
+    public class ProductsTests
+    {
+        [TestMethod]
+        public void Create_Product_Returns_ActionResult()
+        {
+            ProductsController controller = new ProductsController();
+            ActionResult result = controller.Create() as ActionResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ActionResult));
+        }
+
+
+    }
+}
